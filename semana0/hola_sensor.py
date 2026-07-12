a63a8177f37e0a72f0e1ddf0c5b5b0871bb3da21
Martin Contreras <martincb858@gmail.com>
2026-07-12 01:22:20 -0600

    feat: checkpoint semana 0 - sensor minimo con test

--- semana0/hola_sensor.py
+++ semana0/hola_sensor.py
@@ -1,6 +1,7 @@
 class Sensor:
     def __init__(self, sensor_id: str) -> None:
         self.sensor_id = sensor_id
+
  
     def read(self) -> float:
         return 23.5  # valor fijo por ahora; en la semana 3 sera una API real

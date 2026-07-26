25cb1bda6dbe68e0b56ba1d846082d6d25cc832b
Martin Contreras <martincb858@gmail.com>
2026-07-26 01:47:16 -0600

    feat: se agrega alert_manager (GREEN) - US-5

---
+++ semana2/eval/src/alert_manager.py
@@ -0,0 +1,19 @@
+from datetime import datetime, timedelta
+
+
+class AlertManager:
+    def __init__(self) -> None:
+        self.throttling_window = timedelta(minutes=5)
+
+        self._last_alert_time: Dict[str, datetime] = {}
+
+    def send_alert(self, sensor_id: str, alerta: str, valor: float, 
+                   timestamp: datetime) -> None:
+        last_time = self._last_alert_time.get(sensor_id)
+
+        if last_time is not None and (timestamp - last_time) < self.throttling_window:
+            pass
+        else:
+            print(
+                f"[ALERTA] Nodo {sensor_id}: {alerta} detectada con lectura de {valor}")
+            self._last_alert_time[sensor_id] = timestamp

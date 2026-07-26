98b055a8f58dac20d14a6970aed09d50fdb11e28
Martin Contreras <martincb858@gmail.com>
2026-07-26 02:12:53 -0600

    refactor: se simplifica y mejora anomaly_detector

--- semana2/eval/src/anomaly_detector.py
+++ semana2/eval/src/anomaly_detector.py
@@ -1,4 +1,11 @@
 from src.sensor_reading import SensorReading
+from enum import Enum
+
+class AnomalyType(str, Enum):
+    NORMAL = "OPERACION_NORMAL"
+    TEMPERATURA = "ANOMALIA_TEMPERATURA"
+    HUMEDAD = "ANOMALIA_HUMEDAD"
+    TEMPERATURA_Y_HUMEDAD = "ANOMALIA_TEMPERATURA_Y_HUMEDAD"
 
 class AnomalyDetector:
     def __init__(self, temp_threshold: float, hum_threshold: float):
@@ -6,8 +13,16 @@ class AnomalyDetector:
         self.hum_threshold = hum_threshold
 
     def evaluate(self, reading: SensorReading) -> str:
-        if reading.temperatura > self.temp_threshold:
-            return "ANOMALIA_TEMPERATURA"
-        else:
-            return "OPERACION_NORMAL"
+        temp_alta = reading.temperatura > self.temp_threshold
+        hum_alta = reading.humedad > self.hum_threshold
 
+        # Evaluamos la tabla de verdad (temp_alta, hum_alta)
+        match (temp_alta, hum_alta):
+            case (True, True):
+                return AnomalyType.TEMPERATURA_Y_HUMEDAD
+            case (True, False):
+                return AnomalyType.TEMPERATURA
+            case (False, True):
+                return AnomalyType.HUMEDAD
+            case (False, False):
+                return AnomalyType.NORMAL

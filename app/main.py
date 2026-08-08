53af530ada02b5861b5dffc66abae169449110b6
Martin Contreras <martincb858@gmail.com>
2026-08-08 13:05:04 -0600

    feat: se modifica para aceptar db de alembic

--- app/main.py
+++ app/main.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI
 
-from app.db import Base, engine
 from app.routers import health_router, reading_router, sensor_router
 
-Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="SensorHub API",
